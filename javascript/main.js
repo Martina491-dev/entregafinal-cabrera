9608b2f7a8a0fb08eab29b83b78962ba6fe2242b
Martina491 <marticabrera28@gmail.com>
2023-10-18 14:20:10 -0300

    filtrado

--- javascript/main.js
+++ javascript/main.js
@@ -1,120 +1,89 @@
-//tarifas
-const rates = {
-  economico: 30,
-  estandar: 50,
-  lujo: 100,
-};
-
 //Autos
-const autosClasicos = ['Ford Mustang', 'Eleanor Pony', 'Lamborghini Miura', 'Aston Martin', 'Jaguar E-Type'];
+const listaProductos = [
+  { id: 1, nombre: 'Eleanor pony', precio: 100000, stock: 2 },
+  { id: 2, nombre: 'Mustang', precio: 200000, stock: 3 },
+  { id: 3, nombre: 'Aston Martin', precio: 300000, stock: 1 },
+  { id: 4, nombre: 'Jaguar E-Type', precio: 400000, stock: 2 },
+  { id: 5, nombre: 'Lamborghini Miura', precio: 500000, stock: 4 },
+];
 
-// Función para calcular el costo total
-function calculateTotal(selectedRate, rentalDays) {
-  const rate = rates[selectedRate] || 0;
-  return rate * rentalDays;
+// Función para encontrar un producto por ID
+function findProductById(id) {
+  return listaProductos.find(producto => producto.id === id);
 }
 
-// Función para mostrar el menú y obtener la opción del usuario
-function mostrarMenu() {
-  console.log("Menú de opciones:");
-  console.log("1. Ver automóviles disponibles");
-  console.log("2. Hacer una reserva");
-  console.log("3. Ver historial de alquileres");
-  console.log("4. Salir");
-  const opcion =parseInt(prompt("Por favor, seleccione una opción :\n 1) Ver autos disponibles \n 2) Hacer una reserva \n 3) Ver historial de alquiler \n 4) Salir\n"));
-  return parseInt(opcion);
+// Función para filtrar productos por precio máximo
+function filterProductsByPriceMax(maxPrice) {
+  return listaProductos.filter(producto => producto.precio <= maxPrice);
 }
 
-// Función para interactuar con el cliente
-function runCarRentalSimulator() {
-const nombreIngresado = prompt("Ingresar nombre").toLocaleLowerCase();
-  alert("Bienvenido a Classy Cars, " + nombreIngresado + ". Aquí podrás encontrar el mejor auto y precio de alquiler. Presione 'aceptar' o 'enter' para ver las mejores opciones de autos.");
+
+// Función para alquilar un producto
+function alquilarProducto(id) {
+  const product = findProductById(id);
+  if (product) {
+    if (product.stock > 0) {
+      product.stock--;
+      return `Producto alquilado - ID: ${product.id}, Nombre: ${product.nombre}, Precio: $${product.precio}`;
+    } else {
+      return `Producto sin stock disponible.`;
+    }
+  }
+  return `Producto con ID ${id} no encontrado.`;
+}
+
+// Función principal
+function rentCar() {
+  const nombreUsuario = prompt("¡Bienvenido Classy Cars! Por favor, ingrese su nombre:");
+
+  alert(`Hola, ${nombreUsuario}.\nBienvenido a la mejor concesionaria de autos clásicos.`);
+
   let continuar = true;
 
-  let opcionSeleccionada;
+  while (continuar) {
+    const opcion = prompt("Elija una opción:\n1. Ver lista de productos\n2. Alquilar un producto \n3. Filtar por precio \n4. Salir");
 
-while (continuar) {
-  opcionSeleccionada = mostrarMenu();
-
-  switch (opcionSeleccionada) {
-    case 1:
-      console.log("Automóviles disponibles:");
-      alert("Automóviles disponibles:\n" + autosClasicos.join("\n"));
-      break;
-
-    case 2:
-      // Hacer una reserva
-      console.log("Por favor, seleccione un automóvil:");
-      for (const key in autosClasicos) {
-        if (autosClasicos.hasOwnProperty(key)) {
-          console.log(`${key}: ${autosClasicos[key]}`);
+    switch (opcion) {
+      case '1':
+        let lista = 'Lista de productos:\n';
+        for (const producto of listaProductos) {
+          lista += `ID: ${producto.id}, Nombre: ${producto.nombre}, Precio: $${producto.precio}, Stock: ${producto.stock}\n`;
         }
-      }
-    
-      const selectedCarNumber = parseInt(prompt("Seleccione un automóvil \n1)Ford Mustang, \n2)Eleanor Pony, \n3)Lamborghini Miura, \n4) Aston Martin, \n5)Jaguar E-Type \n6) Salir"));
-      
-      if (selectedCarNumber === 6) {
-        alert("Saliendo del simulador. ¡Hasta luego!");
-        continuar = true;
-      } else if (selectedCarNumber >= 1 && selectedCarNumber <= autosClasicos.length) {
-        const selectedCarNumber = autosClasicos[selectedCarNumber - 1];
-      } else {
-        alert("Opción no válida. Por favor, seleccione un automóvil válido.");
-      
-    
+        alert(lista);
+        break;
 
-        // SERVICIO QUE SE ELIGE
-        console.log("Seleccione el tipo de servicio: 1) económico, 2) estándar, 3) lujo");
-        const selectedServiceInput = parseInt(prompt("Tipo de servicio: \n1) económico 30USD, \n2) estándar 50USD, \n3) lujo 100USD"));
+      case '2':
+        const productId = parseInt(prompt("Por favor, ingrese el ID (1-5) del producto que desea alquilar:"));
+        const result = alquilarProducto(productId);
+        alert(result);
+        break;
 
-        let selectedService;
-
-        switch (selectedServiceInput) {
-          case 1:
-            selectedService = 'economico';
-            break;
-          case 2:
-            selectedService = 'estandar';
-            break;
-          case 3:
-            selectedService = 'lujo';
-            break;
-          default:
-            alert("Tipo de servicio no válido. Por favor, seleccione un servicio nuevamente.");
-            return;
+      case '3':
+        const maxPrice = parseFloat(prompt("Ingrese el precio máximo:"));
+        if (!isNaN(maxPrice)) {
+          const productosFiltrados = filterProductsByPriceMax(maxPrice);
+          if (productosFiltrados.length > 0) {
+            let lista = 'Productos dentro del rango de precio:\n';
+            for (const producto of productosFiltrados) {
+              lista += `ID: ${producto.id}, Nombre: ${producto.nombre}, Precio: $${producto.precio}, Stock: ${producto.stock}\n`;
+            }
+            alert(lista);
+          } else {
+            alert("No se encontraron productos dentro del rango de precio especificado.");
+          }
+        } else {
+          alert("Precio no válido. Por favor, ingrese un número válido.");
         }
+        break;
+      case '4':
+        continuar = false;
+        alert('¡Gracias por visitar Classy Cars!');
+        break;
 
-        // DIAS DE ALQUILER
-        const rentalDays = parseInt(prompt("Número de días de alquiler:"));
-        if (isNaN(rentalDays) || rentalDays <= 0) {
-          alert("Número de días no válido. Por favor, ingrese nuevamente un número de días de alquiler.");
-          return
-        }
-
-        // Calculo final
-        const totalCost = calculateTotal(selectedService, rentalDays);
-        alert(`${nombreIngresado}, reservaste un ${selectedCar} (${selectedService}) por ${rentalDays} días. El costo total del alquiler es: $${totalCost}`);
-        alert("Opción no válida. Por favor, seleccione un automóvil válido.");
-        continuar = true;
-      }
-  
-    
-      return;
-      
-
-    case 3:
-      // Aquí puedes agregar la lógica para ver el historial de alquileres.
-      break;
-
-    case 4:
-      alert("Saliendo del simulador. ¡Hasta luego!");
-    return;
-
-    default:
-      alert("Opción no válida. Por favor, seleccione una opción válida.");
-      return;
+      default:
+        alert('Opción no válida. Por favor, seleccione una opción válida.');
+    }
   }
 }
-}
 
-runCarRentalSimulator();
+rentCar();
