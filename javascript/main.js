e9477818d0bb24c5e609aa074746edc4a6016ed4
Martina491 <marticabrera28@gmail.com>
2023-11-06 06:47:57 -0300

    arreglos de fechas

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,8 @@
-const fechaRetiro= "10-11-2023"; "15-11-2023"; "20-11-2023"; "25-11-2023"; "30-11-2023"; "05-12-2023";
-const fechaDevolucion= "15-11-2023"; "20-11-2023"; "25-11-2023"; "30-11-2023"; "05-12-2023"; "10-12-2023";
+const modelosAutos = ["Jaguar E-Type 60", "Ford Mustang", "Chevrolet Camaro", "Aston Martin"];
+const fechasRetiro = ["10-11-2023", "15-11-2023", "20-11-2023", "25-11-2023", "30-11-2023", "05-12-2023"];
+const fechasDevolucion = ["15-11-2023", "20-11-2023", "25-11-2023", "30-11-2023", "05-12-2023", "10-12-2023"];
+const ubicacionesRetiro = ["Aeropuerto de Palermo", "Centro de la Ciudad", "Estación de Tren Roca", "Oficina Local en Puerto Madero"];
+
 document.getElementById("guardarBtn").addEventListener("click", function() {
   const selectedModel = document.getElementById("modelo").value;
   const selectedFechaRetiro = document.getElementById("fecha-retiro").value;
@@ -7,67 +10,68 @@ document.getElementById("guardarBtn").addEventListener("click", function() {
   const selectedUbicacionRetiro = document.getElementById("ubicacion-retiro").value;
   const userEmail = document.getElementById("email").value;
 
-  if (selectedModel === "" || selectedFechaRetiro === "" || selectedFechaDevolucion === "" || selectedUbicacionRetiro === "" || userEmail === "") {
+  if (
+    selectedFechaRetiro === "" || 
+    selectedFechaDevolucion === "" || 
+    userEmail === ""
+  ) {
     Swal.fire({
       icon: 'error',
       title: 'Error',
       text: 'Por favor, complete todos los campos antes de guardar la selección.',
     });
-  } else if (new Date(selectedFechaRetiro) > new Date(selectedFechaDevolucion)) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Error',
-      text: 'La fecha de retiro no puede ser posterior a la fecha de devolución.',
-    });
   } else {
-    const reservationDetails = {
-      model: selectedModel,
-      fechaRetiro: selectedFechaRetiro,
-      fechaDevolucion: selectedFechaDevolucion,
-      ubicacionRetiro: selectedUbicacionRetiro,
-      email: userEmail,
-    };
+    const fechaRetiroDate = new Date(selectedFechaRetiro);
+    const fechaDevolucionDate = new Date(selectedFechaDevolucion);
 
-    let reservations = JSON.parse(localStorage.getItem("reservations")) || [];
-    reservations.push(reservationDetails);
-
-    localStorage.setItem("reservations", JSON.stringify(reservations));
-
-    Swal.fire({
-      icon: 'success',
-      title: 'Reserva realizada con éxito',
-      text: 'En la brevedad le estaremos enviando detalles por email.',
-    });
-  }
-});
-
-window.addEventListener("load", function() {
-  const reservationsString = localStorage.getItem("reservations");
-  if (reservationsString) {
-    const reservations = JSON.parse(reservationsString);
-    const lastReservation = reservations[reservations.length - 1]; //última reserva 
-
-    const mensajeElement = document.getElementById("mensaje");
-    mensajeElement.innerHTML = ''; 
-
-    if (lastReservation) {
-      const messagePara = document.createElement("p");
-      messagePara.textContent = "Última reserva guardada:";
-      mensajeElement.appendChild(messagePara);
-
-      const details = [
-        "Modelo: " + lastReservation.model,
-        "Fecha de Retiro: " + lastReservation.fechaRetiro,
-        "Fecha de Devolución: " + lastReservation.fechaDevolucion,
-        "Ubicación de Retiro: " + lastReservation.ubicacionRetiro,
-        "Correo Electrónico: " + lastReservation.email
-      ];
-
-      details.forEach(detail => {
-        const p = document.createElement("p");
-        p.textContent = detail;
-        mensajeElement.appendChild(p);
+    if (fechaRetiroDate >= fechaDevolucionDate) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'La fecha de retiro no puede ser igual o posterior a la fecha de devolución.',
       });
+    } else {
+      const reservationDetails = {
+        model: selectedModel,
+        fechaRetiro: selectedFechaRetiro,
+        fechaDevolucion: selectedFechaDevolucion,
+        ubicacionRetiro: selectedUbicacionRetiro,
+        email: userEmail,
+      };
+      let reservations = JSON.parse(localStorage.getItem("reservations")) || [];
+      reservations.push(reservationDetails);
+      localStorage.setItem("reservations", JSON.stringify(reservations));
+      Swal.fire({
+        icon: 'success',
+        title: 'Reserva realizada con éxito',
+        text: 'En breve le enviaremos detalles por correo electrónico.',
+      });
+
+      // Actualizar la última reserva
+      updateLastReservation(reservationDetails);
     }
   }
 });
+
+// Función para actualizar la última reserva 
+function updateLastReservation(reservation) {
+  const mensajeElement = document.getElementById("mensaje");
+  mensajeElement.innerHTML = '';
+  
+  const messagePara = document.createElement("p");
+  messagePara.textContent = "Última reserva guardada:";
+  mensajeElement.appendChild(messagePara);
+
+  const details = [
+    "Modelo: " + reservation.model,
+    "Fecha de Retiro: " + reservation.fechaRetiro,
+    "Fecha de Devolución: " + reservation.fechaDevolucion,
+    "Ubicación de Retiro: " + reservation.ubicacionRetiro,
+    "Correo Electrónico: " + reservation.email
+  ];
+  details.forEach(detail => {
+    const p = document.createElement("p");
+    p.textContent = detail;
+    mensajeElement.appendChild(p);
+  });
+}
